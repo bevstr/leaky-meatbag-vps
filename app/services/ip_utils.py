c55f618121667f9592fb2c753341524ab4d7c0df
bevstr <bevo@bevstr.com>
2025-05-06 12:02:40 +0800

    config

--- app/services/ip_utils.py
+++
@@ -1,8 +0,0 @@
-from flask import request
-
-def get_client_ip():
-    return (
-        request.headers.get("X-Real-IP")
-        or request.headers.get("X-Forwarded-For")
-        or request.remote_addr
-    )
